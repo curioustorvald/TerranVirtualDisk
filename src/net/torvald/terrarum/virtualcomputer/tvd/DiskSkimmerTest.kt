e7a300d6013c8ad1415c21a3219d7c0dd5597b7f
Minjae Song <alswo9628@gmail.com>
2018-11-22 23:29:12 +0900

    test suite No 0

--- src/net/torvald/terrarum/virtualcomputer/tvd/DiskSkimmerTest.kt
+++ src/net/torvald/terrarum/virtualcomputer/tvd/DiskSkimmerTest.kt
@@ -1,4 +1,23 @@
 package net.torvald.terrarum.virtualcomputer.tvd
 
-class DiskSkimmerTest {
+import java.io.File
+
+object DiskSkimmerTest {
+
+    val fullBattery = listOf(
+            { invoke00() }
+    )
+
+    operator fun invoke() {
+        fullBattery.forEach { it.invoke() }
+    }
+
+    /**
+     * Testing of DiskSkimmer
+     */
+    fun invoke00() {
+        val infile = File("./test-assets/tevd-test-suite-00.tevd")
+        val outfile = File("./test-assets/tevd-test-suite-00_results.tevd")
+    }
+
 }
